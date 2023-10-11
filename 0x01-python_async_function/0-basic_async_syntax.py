e4626088c58ab351232a123ae53a63d6ff1da58b
NyatiZw <appsmaid19@gmail.com>
2023-10-11 19:06:15 +0000

    initial commit

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,10 +10,12 @@ import asyncio
     Return: integer value
 """
 
+
 async def wait_random(max_delay=10):
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
 
 async def main():
     result = await wait_random()
