b7a328a7b9944da90e1bee962fd5e2d22e0814cc
NyatiZw <appsmaid19@gmail.com>
2023-10-11 19:01:13 +0000

    initial commit

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import random
+import asyncio
+
+""" asynchronous coroutine
+    Args:
+        max_delay: integer
+        wait_random: integer
+
+    Return: integer value
+"""
+
+async def wait_random(max_delay=10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+async def main():
+    result = await wait_random()
+    print(f"Waited for {result:2f} seconds")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
