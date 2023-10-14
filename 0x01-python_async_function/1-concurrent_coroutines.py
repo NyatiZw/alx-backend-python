7d365ce8003a909d7a9a96439576f96f146a3b89
NyatiZw <appsmaid19@gmail.com>
2023-10-14 10:42:22 +0000

    running pycodestyle check on file

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -23,6 +23,7 @@ async def wait_random(max_delay: int = 10) -> float:
     await asyncio.sleep(delay)
     return delay
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawn `wait_random` n times with the specified `max_delay`.
@@ -36,7 +37,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     delays: List[float] = [0] * n
 
-
     async def run_wait_random(i: int):
         delay = await wait_random(max_delay)
         delays[i] = delay
@@ -44,6 +44,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     tasks = [run_wait_random(i) for i in range(n)]
     await asyncio.gather(*tasks)
     return delays
+
 
 async def main():
     n = 5
