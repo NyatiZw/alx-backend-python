2b0a0760dc316344ebcc33f622f69dcc61ada12c
NyatiZw <appsmaid19@gmail.com>
2023-10-14 09:42:26 +0000

    submitting multiple coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+This module provides an asynchronous coroutine for waiting a random delay.
+"""
+
+
+import asyncio
+import random
+from typing import List
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    asynchronous coroutine
+
+    Args:
+        max_delay (float): The maximum delay in seconds.
+
+    Returns:
+        float: The actual delay in seconds.
+    """
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+async def wait_n(n: int, max_delay: float) -> List[float]:
+    """
+    Spawn `wait_random` n times with the specified `max_delay`.
+
+    Args:
+        n (int): The number of times to run `wait_random`.
+        max_delay (float): The maximum delay in seconds for each call.
+
+    Returns:
+        List[float]: A list of delays in ascending order.
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return delays
+
+
+async def main():
+    n = 5
+    max_delay = 3.0
+    result = await wait_n(n, max_delay)
+    result.sort()
+    print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
