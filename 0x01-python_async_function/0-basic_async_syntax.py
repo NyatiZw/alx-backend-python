92fc8ce3c6d681c37c37ae2eab475bdf6447e6ae
NyatiZw <appsmaid19@gmail.com>
2023-10-14 09:06:31 +0000

    fixing missing max_delay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+This module provides an asynchronous coroutine for waiting a random delay.
+"""
+
 import random
 import asyncio
 
@@ -13,18 +17,18 @@ async def wait_random(max_delay: float = 10) -> float:
     Returns:
         float default value
     """
-
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
 
 
-async def main() -> None:
+async def main():
     """
     Main asynchronous function to run coroutine.
     """
-    result = await wait_random()
-    print(f"Waited for {result:2f} seconds")
+    max_delay = 5
+    result = await wait_random(max_delay)
+    print(f"Waited for {result:.2f} seconds")
 
 
 if __name__ == "__main__":
