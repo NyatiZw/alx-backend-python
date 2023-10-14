7647aa4d712324b1b01b9c569a8562999e1aa393
NyatiZw <appsmaid19@gmail.com>
2023-10-14 10:39:53 +0000

    modified 1-concurrent_coroutine

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,23 +9,21 @@ import random
 from typing import List
 
 
-
-async def wait_random(max_dalay: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     asynchronous coroutine
 
     Args:
-        max_delay (float): The maximum delay in seconds.
+        max_delay (float): The maximum delay in seconds
 
     Returns:
-        float: The actual delay in seconds.
+        float: The actual delay in seconds
     """
-    delay: float = random.uniform(0, max_dalay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
 
-
-async def wait_n(n: int, max_delay: float) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawn `wait_random` n times with the specified `max_delay`.
 
@@ -36,14 +34,20 @@ async def wait_n(n: int, max_delay: float) -> List[float]:
     Returns:
         List[float]: A list of delays in ascending order.
     """
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return delays
+    delays: List[float] = [0] * n
 
+
+    async def run_wait_random(i: int):
+        delay = await wait_random(max_delay)
+        delays[i] = delay
+
+    tasks = [run_wait_random(i) for i in range(n)]
+    await asyncio.gather(*tasks)
+    return delays
 
 async def main():
     n = 5
-    max_delay = 3.0
+    max_delay = 5.0
     result = await wait_n(n, max_delay)
     print(result)
 
