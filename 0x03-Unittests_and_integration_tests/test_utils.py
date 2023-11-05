1dfccff708da60549e66c8b0aea46616a9e1db8a
NyatiZw <appsmaid19@gmail.com>
2023-11-05 13:41:25 +0000

    fixing pycodestyle error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,12 +9,23 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    A test class for the access_nested_map function.
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
+        """
+        Test the access_nested_map function with various inputs
+
+        Parameters:
+            nested_map (dict): The nested map to access.
+            path (tuple): The path to access in the map.
+            expected_result (any): Expected result
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
@@ -26,6 +37,14 @@ class TestAccessNestedMap(unittest.TestCase):
             nested_map,
             path,
             expected_exception_message):
+        """
+        Test that the access_nested_map function raises a KeyError
+
+        Parameters:
+            nested_map (dict): The nested map to access.
+            path (tuple): The path to access in the map.
+            expected_exception_message (str): The expected error.
+        """
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
 
